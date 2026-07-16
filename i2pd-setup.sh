ba3698152d4315ecd2bc91f2ba337554a270b769
Plasmoid28 <plasmoid28@noreply.codeberg.org>
2026-07-16 19:07:42 +0200

    Update i2pd-setup.sh

--- i2pd-setup.sh
+++ i2pd-setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export DEBIAN_FRONTEND=noninteractive
+export NEEDRESTART_MODE=a
+
 if [[ $EUID -ne 0 ]]; then
   echo "Run as root: sudo bash i2pd-setup.sh [PORT]" >&2
   exit 1
